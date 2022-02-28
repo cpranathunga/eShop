b7f3e682232c70e178f829c9da3f3447404420d4
Chandima Ranathunga <chandima.ranathunga@blackswan-technologies.com>
2022-02-28 10:38:10 +0530

    product service added

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProductService {
 
-  constructor() { }
+  constructor(private $http: HttpClient) { }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.$http.get<Product[]>('http://localhost:8080/products?_page=1&_limit=10')
+  }
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,5 +1,7 @@
+import { ProductService } from './product.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -7,12 +9,14 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-  
+
     ProductListComponent
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
-  ]
+    ProductsRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [ProductService]
 })
 export class ProductsModule { }
